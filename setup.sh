a651b00db1e1648900566aa49d94e9162703e3d2
Hunter Kavanagh <hunter.kavanagh@protonmail.com>
2024-04-23 11:24:30 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,57 @@
+# Install Nala
+sudo apt install nala
+
+
+# Get Suckless stuff
+mkdir ~/Suckless
+cd ~/Suckless
+
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/st
+
+
+# Install Suckless Dependancies
+sudo nala install xorg
+
+
+# Make Suckless stuff
+cd ~/Suckless/dwm
+sudo make clean install
+
+cd ~/Suckless/dmenu
+sudo make clean install 
+
+cd ~/Suckless/st
+sudo make clean install 
+
+
+# Add .xinitrc
+echo 'exec dwm' > ~/.xinitrc
+
+
+# Install librewolf
+sudo apt update && sudo apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
+
+distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
+
+wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+
+sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: $distro
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
+
+sudo apt update
+sudo nala install librewolf -y
+
+
+# Install text editors
+sudo nala install micro vscode
+
+
+# Configure Audio
